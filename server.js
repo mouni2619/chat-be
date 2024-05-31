db3fef412e9917d1dd71531fdf3e7a57d242a607
mouni2619 <mounikaadada26@gmail.com>
2024-05-31 20:47:48 +0530

    root endpoint

--- server.js
+++ server.js
@@ -34,7 +34,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
-
+app.get('/', (req, res) => {
+    res.send('Welcome to the root endpoint!');
+  });
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
