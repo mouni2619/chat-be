b7bbd378bc4f3a81d1043c08f11b17ace2e09556
mouni2619 <mounikaadada26@gmail.com>
2024-05-31 20:36:43 +0530

    frontend url

--- socket/socket.js
+++ socket/socket.js
@@ -7,7 +7,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["http://localhost:3000"],
+		origin: ["https://chat-fe-five.vercel.app/login"],
 		methods: ["GET", "POST"],
 	},
 });
