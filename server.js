b363b00edb5991e485ba724584ff69bf926232ac
mouni2619 <mounikaadada26@gmail.com>
2024-06-02 00:12:54 +0530

    change directory

--- server.js
+++ server.js
@@ -34,6 +34,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
 app.get('/', (req, res) => {
     res.send('Welcome to the root endpoint!');
   });
@@ -41,7 +43,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
