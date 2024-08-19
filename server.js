17b1a29530c2a43b64075fc5edb224855569b3f9
mouni2619 <mounikaadada26@gmail.com>
2024-08-20 00:54:09 +0530

    cors updated

--- server.js
+++ server.js
@@ -2,19 +2,17 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import { writeFileSync } from "fs";
-
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 import crypto from 'crypto';
+import cors from "cors";  // Import the cors package
 
 
 dotenv.config();
-
 
 // Check if JWT_SECRET is not set
 if (!process.env.JWT_SECRET) {
@@ -29,26 +27,35 @@ if (!process.env.JWT_SECRET) {
 }
 
 const __dirname = path.resolve();
-// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+// Initialize the Express app
+app.use(cors({
+    origin: 'http://localhost:3000', // Replace with your frontend's origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get('/', (req, res) => {
     res.send('Welcome to the root endpoint!');
-  });
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
-server.listen(PORT, () => {
-	connectToMongoDB();
-	console.log(`Server Running on port ${PORT}`);
+server.listen(PORT, async () => {
+    try {
+        await connectToMongoDB();
+        console.log(`Server Running on port ${PORT}`);
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error.message);
+    }
 });
